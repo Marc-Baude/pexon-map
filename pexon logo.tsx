254735c0e643f9c073cea4216d0732981c6ed4e8
Marc Baude <marcbaude@gmail.com>
2022-04-29 22:57:01 +0200

    Add new office

---
+++ pexon logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="logo" tilewidth="116" tileheight="32" tilecount="1" columns="1">
+ <image source="logo_2.webp" trans="ff00ff" width="116" height="39"/>
+</tileset>
